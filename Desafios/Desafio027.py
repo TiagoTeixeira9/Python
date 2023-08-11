727ebcec9fb36de570000a74cc359bdc9aa669f2
Tiago Teixeira <108500810+TiagoTeixeira9@users.noreply.github.com>
2023-08-11 18:12:03 +0700

    Mais desafios feitos

---
+++ Desafios/Desafio027.py
@@ -0,0 +1,5 @@
+n = str(input("Digite o seu nome completo: ")).strip()
+nome = n.split()
+print("Muito prazer em te conhecer !")
+print("O seu primeiro nome é {}".format(nome[0]))
+print("O seu ultimo nome é {}".format(nome[len(nome)-1]))
