d2edec515c0cbcff4ac76f6e403ed294ceffe3bb
Tiago Teixeira <108500810+TiagoTeixeira9@users.noreply.github.com>
2023-08-11 09:56:37 +0700

    Update no exercício003

--- Desafios/Desafio003.py
+++ Desafios/Desafio003.py
@@ -1,4 +1,4 @@
 n1 = int(input("Primeiro número: "))
 n2 = int(input("Segundo número: "))
 s = n1+n2
-print("A soma entre {} e {} vale {}".format(n1, n2, s))
+print("A soma entre {} e {} é igual a {}.".format(n1, n2, s))
