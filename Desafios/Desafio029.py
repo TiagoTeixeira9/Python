0d2e710053d055984cd723414258e2783383df53
Tiago Teixeira <108500810+TiagoTeixeira9@users.noreply.github.com>
2023-08-12 21:49:20 +0700

    Update no desafio029

--- Desafios/Desafio029.py
+++ Desafios/Desafio029.py
@@ -1,9 +1,9 @@
 velocidade = float(input("Qual é a velocidade atual do carro? "))
 m = 1*10
-if velocidade <= 80:
+if velocidade > 80:
     print("MULTADO! Você excedeu o limite permitido que é 80KM/h")
-    multa = (velocidade-80) * 7
-    print("Tens que pagar uma multa de {}US$".format(m))
+    multa = (velocidade-80) * 1
+    print("Tens que pagar uma multa de {:.2f}US$".format(multa))
     print("Tenha um bom dia! Dirija com mais segurança !")
-
-print("Tenha um bom dia! Dirija com segurança !")
+else:
+    print("Tenha um bom dia! Dirija com segurança !")
