66a6a04e492032a037708dc5a0467aa57bdde8a4
Tiago Teixeira <108500810+TiagoTeixeira9@users.noreply.github.com>
2023-08-13 14:48:06 +0700

    Create aula011

---
+++ Aulas/Aula011.py
@@ -0,0 +1,6 @@
+nome = "Tiago"
+cores = {"limpa": "\033[m",
+         "azul": "\033[34m",
+         "amarelo": "\033[33m",
+         "pretoebranco": "\033[7;30m"}
+print("Olá! Muito prazer em te conhecer, {}{}{}!!!".format(cores["azul"], nome, cores["limpa"]))
